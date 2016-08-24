a680a8c95e6d67291fd1cc182d2fb32227792401
Tomaž Curk <tomazc@gmail.com>
2016-08-24 11:15:35 +0200

    Discard too short sequences after barcode removal and adapter trimming.

--- iCount/externals/cutadapt.py
+++ iCount/externals/cutadapt.py
@@ -6,7 +6,9 @@ import subprocess
 
 
 def get_version():
-    args = ['cutadapt', '--version']
+    args = [
+        'cutadapt', '--version'
+    ]
     try:
         ver = subprocess.check_output(args, shell=False,
                                       universal_newlines=True)
@@ -15,11 +17,17 @@ def get_version():
         return None
 
 
-def run(in_fname, out_fname, adapter, qual_base=64, qual_trim=None):
-    args = ['cutadapt', '--quiet', '-a', adapter,
-            '--quality-base=%s' % qual_base]
+def run(in_fname, out_fname, adapter, qual_base=64, qual_trim=None,
+        minimum_length=None):
+    args = [
+        'cutadapt', '--quiet',
+        '-a', adapter,
+        '--quality-base={:d}'.format(qual_base)
+    ]
     if qual_trim is not None:
-        args.extend(['-q', '%s' % qual_trim])
+        args.extend(['-q', '{:d}'.format(qual_trim)])
+    if minimum_length is not None:
+        args.extend(['-m', '{:d}'.format(minimum_length)])
     args.extend(['-o', out_fname, in_fname])
 
     return subprocess.call(args, shell=False)
