29389296009410c57433fc7fb11413fe6556b651
Jure Zmrzlikar <zmrzlikar.jure@gmail.com>
2016-09-27 18:06:42 +0200

    Refactor test_demultiplex, relates to #40

--- iCount/tests/test_demultiplex.py
+++ iCount/tests/test_demultiplex.py
@@ -2,71 +2,43 @@ import os
 import shutil
 import unittest
 
-from . import test_data
-import iCount
-
-files_to_get = [
-    (
-        'LUjh03.fq.gz',
-        'http://icount.fri.uni-lj.si/data/20101116_LUjh03/SLX-2605'
-        '.CRIRUN_501.s_4.sequence.reduced.txt.gz'
-    ),
-]
-
-
-def setUpModule():
-    test_data.get_update(files_to_get)
-
-
-def tearDownModule():
-    pass
-
-
-test_output_folder = os.path.join(test_data.test_folder, 'runs', 'demultiplex')
-if not os.path.exists(test_output_folder):
-    print("Test folder does not exist. Will create it at: "
-          "{0:s}".format(test_output_folder))
-    os.makedirs(test_output_folder)
+from iCount import demultiplex
+from iCount.tests.utils import make_fastq_file, make_list_from_file, get_temp_dir
 
 
 class TestDemultiplex(unittest.TestCase):
 
     def setUp(self):
-        if os.path.isdir(test_output_folder):
-            print('Removing folder: {:s}'.format(test_output_folder))
-            shutil.rmtree(test_output_folder)
-            print('Creating folder: {:s}'.format(test_output_folder))
-            os.makedirs(test_output_folder)
-
-    def test_demultiplex(self):
-        in_fastq_fname = os.path.join(test_data.test_data_folder,
-                                      'LUjh03.fq.gz')
-
-        # extract
-        # list all experiment barcodes
-        barcodes = [
+        self.dir = get_temp_dir()
+        self.adapter = 'CCCCCCCCC'
+        self.barcodes = [
             'NNNGGTTNN',
             'NNNTTGTNN',
             'NNNCAATNN',
             'NNNACCTNN',
             'NNNGGCGNN',
         ]
+        self.reads = make_fastq_file(barcodes=self.barcodes, adapter=self.adapter)
 
-        out_fastq_fnames = []
-        for bc in barcodes:
-            out_fastq_fnames.append(
-                os.path.join(test_output_folder, 'demulti.{}.fastq'.format(bc))
-            )
-        not_matching_fastq_fname = os.path.join(test_output_folder,
-                                                'nomatch.fastq')
+    def test_run_fail(self):
+        message = r'Output directory does not exist. Make sure it does.'
+        with self.assertRaisesRegex(FileNotFoundError, message):
+            filenames = demultiplex.run(
+                self.reads, self.adapter, 1, self.barcodes, outdir='/unexisting/dir')
 
-        iCount.demultiplex.demultiplex(
-            in_fastq_fname,
-            out_fastq_fnames,
-            not_matching_fastq_fname,
-            barcodes,
-            mismatches=1,
-        )
+    def test_run_ok(self):
+        expected = ['{}/demux_{}.fastq.gz'.format(self.dir, b) for b in self.barcodes]
+        expected.extend([self.dir + '/demux_nomatch.fastq.gz'])
+
+        # Without adapter
+        filenames = demultiplex.run(
+            self.reads, None, 1, self.barcodes, outdir=self.dir)
+        self.assertEqual(sorted(filenames), sorted(expected))
+
+        # With adapter
+        filenames = demultiplex.run(
+            self.reads, self.adapter, 1, self.barcodes, outdir=self.dir)
+        self.assertEqual(sorted(filenames), sorted(expected))
 
 
 if __name__ == '__main__':
