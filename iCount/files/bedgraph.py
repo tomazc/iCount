c272a151339b6263a22f0c6eabc4841956fbf513
Jure Zmrzlikar <zmrzlikar.jure@gmail.com>
2018-06-26 09:54:51 +0200

    bedgraph: support additional track-configuring parameters

--- iCount/files/bedgraph.py
+++ iCount/files/bedgraph.py
@@ -7,6 +7,7 @@ Convert from BED6 to bedGraph format.
 
 """
 import logging
+import re
 
 import pybedtools
 
@@ -15,13 +16,14 @@ import iCount
 LOGGER = logging.getLogger(__name__)
 
 
-def bed2bedgraph(bed, bedgraph, name='User Track', description='User Supplied Track'):
+def bed2bedgraph(bed, bedgraph, name='User Track', description='User Supplied Track', visibility=None, priority=None,
+                 color=None, alt_color=None, max_height_pixels=None):
     """
     Convert from BED6 to bedGraph format.
 
-    For further explanation of ``name`` and ``description`` parameters
-    (and also others, that are not yet supported) see:
+    For further explanation of parameters see:
     https://genome.ucsc.edu/goldenPath/help/customTrack.html#TRACK
+    https://genome.ucsc.edu/goldenpath/help/trackDb/trackDbHub.html
 
     Parameters
     ----------
@@ -30,9 +32,24 @@ def bed2bedgraph(bed, bedgraph, name='User Track', description='User Supplied Tr
     bedgraph : str
         Output bedGraph file.
     name : str
-        Track label. Can consist of up to 15 characters.
+        Track label. Should be shorter than 15 characters.
     description : str
-        Track description. Can consist of up to 60 characters.
+        Track description. Should be shorter than 60 characters.
+    visibility: str
+        Define the initial display mode of the annotation track. Choose
+        among "hide", "dense", "full", "pack" and "squish". Default is
+        "dense".
+    priority : int
+        Defines the track's order relative to other tracks in same group.
+    color : str
+        Define the main color for the annotation track. The track color
+        consists of three comma-separated RGB values from 0-255, e.g
+        RRR,GGG,BBB. The default value is 0,0,0 (black).
+    alt_color : str
+        Allow a color range that varies from color to alt_color.
+    max_height_pixels : str
+        The limits of vertical viewing space for track, though it is
+        configurable by the user. Should be of the format <max:default:min>.
 
     Returns
     -------
@@ -42,13 +59,29 @@ def bed2bedgraph(bed, bedgraph, name='User Track', description='User Supplied Tr
     iCount.log_inputs(LOGGER, level=logging.INFO)  # pylint: disable=protected-access
     LOGGER.info('Checking input parameters...')
     assert bed.endswith(('.bed', '.bed.gz'))
+    assert bedgraph.endswith('.bedgraph')
 
     header = [
         'track',
         'type=bedGraph',
-        'name="{}"'.format(name[:15]),
-        'description="{}"'.format(description[:60]),
+        'name="{}"'.format(name),
+        'description="{}"'.format(description),
     ]
+    if visibility:
+        assert re.fullmatch(r'hide|dense|full|pack|squish', visibility)
+        header.append('visibility={}'.format(visibility))
+    if priority:
+        assert re.fullmatch(r'\d+', str(priority))
+        header.append('priority={}'.format(priority))
+    if color:
+        assert re.fullmatch(r'\d{1,3},\d{1,3},\d{1,3}', color)
+        header.append('color={}'.format(color))
+    if alt_color:
+        assert re.fullmatch(r'\d{1,3},\d{1,3},\d{1,3}', alt_color)
+        header.append('altColor={}'.format(alt_color))
+    if max_height_pixels:
+        assert re.fullmatch(r'\d{1,3}:\d{1,3}:\d{1,3}', max_height_pixels)
+        header.append('maxHeightPixels={}'.format(max_height_pixels))
 
     LOGGER.info('Converting %s to %s', bed, bedgraph)
     with open(bedgraph, 'wt') as outfile:
