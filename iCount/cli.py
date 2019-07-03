ulelab/iCount-Mini

rnamaps: Completely rework RNA-maps analysis

iCount/cli.py
@@ -255,7 +255,8 @@ def make_parser_from_function(function, subparsers, module=None, only_func=False
         name = function.__name__
         # Take only the docstring until the 'Parameters' section:
         desc = inspect.getdoc(function).split('\n')
-        idx = [i for i, d in enumerate(desc) if d == 'Parameters' or d == 'Returns'][0]
+        idx = [i for i, d in enumerate(desc) if d == 'Parameters' or d == 'Returns']
+        idx = 0 if not idx else idx[0]
         description = '\n'.join(desc[:idx])
         description = remove_comments(description)
     else:
@@ -365,6 +366,14 @@ def main():
         iCount.analysis.rnamaps.run, subparsers)
     make_parser_from_function(
         iCount.analysis.summary.summary_reports, subparsers)
+
+    # Plotting:
+    make_parser_from_function(
+        iCount.plotting.plot_rnamap.plot_rnamap, subparsers)
+    make_parser_from_function(
+        iCount.plotting.plot_rnaheatmap.plot_rnaheatmap, subparsers)
+    make_parser_from_function(
+        iCount.plotting.plot_combined.plot_combined, subparsers)
 
     # File converters:
     make_parser_from_function(iCount.files.bedgraph.bed2bedgraph, subparsers)

iCount/tests/test_e2e.py
@@ -3,12 +3,11 @@ import os
 import unittest
 import warnings
 
-import pysam
 from numpy import random
 
 import iCount
 from iCount.tests.utils import get_temp_file_name, get_temp_dir, make_file_from_list, \
-    make_list_from_file, make_fasta_file, make_sequence, make_quality_scores, attrs
+    make_fasta_file, make_sequence, make_quality_scores, attrs
 
 
 class TestEndToEnd(unittest.TestCase):
@@ -44,20 +43,19 @@ class TestEndToEnd(unittest.TestCase):
             ['1', '.', 'CDS', '400', '499', '.', '+', '.', attrs(gid='G1', tid='T2', exn=2)],
 
             # Gene #2, positive strand:
-            ['1', '.', 'gene', '600', '799', '.', '+', '.', attrs(gid='G2')],
+            ['1', '.', 'gene', '600', '899', '.', '+', '.', attrs(gid='G2')],
             # Transcript #3
-            ['1', '.', 'transcript', '600', '799', '.', '+', '.', attrs(gid='G2', tid='T3')],
-            ['1', '.', 'exon', '600', '649', '.', '+', '.', attrs(gid='G2', tid='T3', exn=1)],
-            ['1', '.', 'CDS', '600', '649', '.', '+', '.', attrs(gid='G2', tid='T3', exn=1)],
-            ['1', '.', 'exon', '750', '799', '.', '+', '.', attrs(gid='G2', tid='T3', exn=2)],
-            ['1', '.', 'CDS', '750', '799', '.', '+', '.', attrs(gid='G2', tid='T3', exn=2)],
+            ['1', '.', 'transcript', '600', '899', '.', '+', '.', attrs(gid='G2', tid='T3')],
+            ['1', '.', 'exon', '600', '659', '.', '+', '.', attrs(gid='G2', tid='T3', exn=1)],
+            ['1', '.', 'CDS', '600', '659', '.', '+', '.', attrs(gid='G2', tid='T3', exn=1)],
+            ['1', '.', 'exon', '840', '899', '.', '+', '.', attrs(gid='G2', tid='T3', exn=2)],
+            ['1', '.', 'CDS', '840', '899', '.', '+', '.', attrs(gid='G2', tid='T3', exn=2)],
 
             # Gene #3, negative strand:
             ['1', '.', 'gene', '800', '899', '.', '-', '.', 'gene_id "G3";'],
             # Transcript #4
             ['1', '.', 'transcript', '800', '899', '.', '-', '.', attrs(gid='G3', tid='T4')],
             ['1', '.', 'exon', '800', '899', '.', '-', '.', attrs(gid='G3', tid='T4', exn=1)],
-            ['1', '.', 'CDS', '800', '899', '.', '-', '.', attrs(gid='G3', tid='T4', exn=1)],
         ])
 
         # Define positions of cross-links:
@@ -108,9 +106,12 @@ class TestEndToEnd(unittest.TestCase):
         From raw reads and ENSEMBL annotation to rnamaps.
         """
 
-        # Make segmentation
+        # Make segmentation & regions file
         seg = get_temp_file_name(extension='gtf')
+        out_dir = get_temp_dir()
         iCount.genomes.segment.get_segments(self.gtf, seg, self.fai)
+        iCount.genomes.segment.make_regions(seg, out_dir)
+        regions = os.path.join(out_dir, iCount.genomes.segment.REGIONS_FILE)
 
         # Build STAR index:
         genome_index = get_temp_dir()
@@ -126,50 +127,12 @@ class TestEndToEnd(unittest.TestCase):
         bam = [fname for fname in os.listdir(map_dir) if fname.startswith('Aligned')][0]
         bam = os.path.join(map_dir, bam)
 
-        # Make all sorts of analysis and save it:
-        normal_out = get_temp_file_name(extension='tsv')
-        strange_out = get_temp_file_name(extension='bam')
-        cross_tr_out = get_temp_file_name(extension='tsv')
-        iCount.analysis.rnamaps.run(bam, seg, normal_out, strange_out, cross_tr_out,
-                                    implicit_handling='split')
-        # Normal output:
-        expected_out = [
-            ['RNAmap', 'type', 'position', 'all', 'explicit'],
-            ['CDS-CDS', '-40', '0.3333', '0'],
-            ['CDS-UTR3', '-25', '0.25', '0'],
-            ['CDS-intergenic', '-20', '1', '1'],
-            ['CDS-intergenic', '30', '0.5', '0'],
-            ['CDS-intergenic', '250', '1', '0'],
-            ['CDS-intron', '-40', '0.3333', '0'],
-            ['CDS-intron', '-25', '0.25', '0'],
-            ['UTR5-CDS', '5', '0.25', '0'],
-            ['UTR5-intron', '-10', '1', '1'],
-            ['UTR5-intron', '-8', '2', '2'],
-            ['UTR5-intron', '10', '0.5', '0'],
-            ['UTR5-intron', '13', '1', '0'],
-            ['intergenic-CDS', '-70', '0.5', '0'],
-            ['intergenic-CDS', '10', '0.3333', '0'],
-            ['intergenic-UTR5', '-20', '1', '1'],
-            ['intron-CDS', '-40', '0.5', '0'],
-            ['intron-CDS', '-37', '1', '0'],
-            ['intron-CDS', '5', '0.25', '0'],
-        ]
-        self.assertEqual(expected_out, make_list_from_file(normal_out))
+        sites_single = get_temp_file_name(extension='bed.gz')
+        sites_multi = get_temp_file_name(extension='bed.gz')
+        skipped = get_temp_file_name(extension='bam')
+        iCount.mapping.xlsites.run(bam, sites_single, sites_multi, skipped)
 
-        # Cross transcript:
-        expected_cross_transcript = [
-            ['chrom', 'strand', 'xlink', 'second-start', 'end-position', 'read_len'],
-            ['1', '+', '234', '236', '284', '50'],
-        ]
-        self.assertEqual(expected_cross_transcript, make_list_from_file(cross_tr_out))
-
-        # Strange:
-        strange_reads = list(pysam.AlignmentFile(strange_out, 'rb'))  # pylint: disable=no-member
-        self.assertEqual(len(strange_reads), 1)
-        strange_read = strange_reads[0]
-        self.assertEqual(strange_read.query_name, 'name_strange:rbc:GGGG')
-        self.assertEqual(strange_read.reference_start, 250)
-        self.assertEqual(strange_read.cigar, [(0, 45), (2, 15), (0, 70)])
+        iCount.analysis.rnamaps.run(sites_single, regions)
 
 
 if __name__ == '__main__':

iCount/tests/test_plotting.py
@@ -1,0 +1,109 @@
+# pylint: disable=missing-docstring, protected-access
+import os
+import unittest
+import warnings
+
+from iCount.plotting import plot_rnamap, plot_rnaheatmap, plot_combined
+from iCount.tests.utils import get_temp_file_name, make_file_from_list
+
+
+def get_example_results_file():
+    """Produce some thest file."""
+    def zeros(size):
+        """Return array of zeros of size ``size``."""
+        return [0 for i in range(size)]
+
+    fname = make_file_from_list(bedtool=False, data=[
+        ['total_cdna:1000000'],
+        ['.'] + [str(pos) for pos in range(-50, 51)],
+        ['chr1_+_100'] + zeros(51) + ['3', '5'] + zeros(48),
+        ['chr1_+_200'] + zeros(49) + ['3', '0', '0', '1'] + zeros(48),
+    ])
+
+    return fname
+
+
+class TestPlotRnaMap(unittest.TestCase):
+
+    def setUp(self):
+        warnings.simplefilter("ignore", (ResourceWarning, ImportWarning))
+
+        self.results_file = get_example_results_file()
+
+    def test_normalize_cpm(self):
+
+        normalized = plot_rnamap.normalize_cpm(1, 10**6)
+        self.assertEqual(normalized, 1)
+
+        normalized = plot_rnamap.normalize_cpm(2, 10**6 * 2)
+        self.assertEqual(normalized, 1)
+
+    def test_parse(self):
+        data, landmark_count = plot_rnamap.parse_results(self.results_file)
+        self.assertEqual(landmark_count, 2)
+
+        expected = {
+            -1: 3.0,
+            1: 3.0,
+            2: 6.0,
+        }
+        for pos, score in data.items():
+            self.assertEqual(score, expected.get(pos, 0.0))
+
+    def test_plot(self):
+        outfile = get_temp_file_name(extension='png')
+        plot_rnamap.plot_rnamap(self.results_file, outfile)
+        self.assertTrue(os.path.isfile(outfile))
+
+
+class TestSmooth(unittest.TestCase):
+
+    def setUp(self):
+        warnings.simplefilter("ignore", (ResourceWarning, ImportWarning))
+
+    def test_smoothe(self):
+        # pylint: disable=missing-docstring, protected-access
+        values = [0, 2, 0]
+        self.assertEqual(plot_rnamap.smooth(values, 1), [1, 2 / 3, 1])
+
+        values = [0, 2, 4, 1, 0]
+        self.assertEqual(plot_rnamap.smooth(values, 1), [1, 2, 7 / 3, 5 / 3, 0.5])
+        self.assertEqual(plot_rnamap.smooth(values, 2), [2, 7 / 4, 7 / 5, 7 / 4, 5 / 3])
+
+
+class TestPlotRnaHeatMap(unittest.TestCase):
+
+    def setUp(self):
+        warnings.simplefilter("ignore", (ResourceWarning, ImportWarning))
+
+        self.results_file = get_example_results_file()
+
+    def test_make_position_to_bin1(self):
+        pos2bins = plot_rnaheatmap.make_position_to_bin([0, 2, 4])
+        self.assertEqual(pos2bins, {0: 0, 1: 0, 2: 2, 3: 2, 4: 2})
+
+    def test_parse(self):
+        data = plot_rnaheatmap.parse_results(self.results_file, -50, 50, top_n=2, binsize=10)
+        self.assertEqual(list(data.index), ['chr1_+_100', 'chr1_+_200'])
+        self.assertEqual(list(data.columns), list(range(-50, 50, 10)))
+        self.assertEqual(data.at['chr1_+_100', 0], 8.0)
+        self.assertEqual(data.at['chr1_+_200', -10], 3.0)
+        self.assertEqual(data.at['chr1_+_200', 0], 1.0)
+
+    def test_plot(self):
+        outfile = get_temp_file_name(extension='png')
+        plot_rnaheatmap.plot_rnaheatmap(self.results_file, outfile, top_n=2, binsize=10)
+        self.assertTrue(os.path.isfile(outfile))
+
+
+class TestPlotRnaCombined(unittest.TestCase):
+
+    def setUp(self):
+        warnings.simplefilter("ignore", (ResourceWarning, ImportWarning))
+
+        self.results_file = get_example_results_file()
+
+    def test_plot(self):
+        outfile = get_temp_file_name(extension='png')
+        plot_combined.plot_combined(self.results_file, outfile, top_n=2, nbins=50)
+        self.assertTrue(os.path.isfile(outfile))

tomazc/iCount

iCount/cli.py
@@ -255,7 +255,8 @@ def make_parser_from_function(function, subparsers, module=None, only_func=False
         name = function.__name__
         # Take only the docstring until the 'Parameters' section:
         desc = inspect.getdoc(function).split('\n')
-        idx = [i for i, d in enumerate(desc) if d == 'Parameters' or d == 'Returns'][0]
+        idx = [i for i, d in enumerate(desc) if d == 'Parameters' or d == 'Returns']
+        idx = 0 if not idx else idx[0]
         description = '\n'.join(desc[:idx])
         description = remove_comments(description)
     else:
@@ -365,6 +366,14 @@ def main():
         iCount.analysis.rnamaps.run, subparsers)
     make_parser_from_function(
         iCount.analysis.summary.summary_reports, subparsers)
+
+    # Plotting:
+    make_parser_from_function(
+        iCount.plotting.rnamap.plot_rnamap, subparsers)
+    make_parser_from_function(
+        iCount.plotting.rnaheatmap.plot_rnaheatmap, subparsers)
+    make_parser_from_function(
+        iCount.plotting.rnacombined.plot_combined, subparsers)
 
     # File converters:
     make_parser_from_function(iCount.files.bedgraph.bed2bedgraph, subparsers)

iCount/tests/test_e2e.py
@@ -3,12 +3,11 @@ import os
 import unittest
 import warnings
 
-import pysam
 from numpy import random
 
 import iCount
 from iCount.tests.utils import get_temp_file_name, get_temp_dir, make_file_from_list, \
-    make_list_from_file, make_fasta_file, make_sequence, make_quality_scores, attrs
+    make_fasta_file, make_sequence, make_quality_scores, attrs
 
 
 class TestEndToEnd(unittest.TestCase):
@@ -44,20 +43,19 @@ class TestEndToEnd(unittest.TestCase):
             ['1', '.', 'CDS', '400', '499', '.', '+', '.', attrs(gid='G1', tid='T2', exn=2)],
 
             # Gene #2, positive strand:
-            ['1', '.', 'gene', '600', '799', '.', '+', '.', attrs(gid='G2')],
+            ['1', '.', 'gene', '600', '899', '.', '+', '.', attrs(gid='G2')],
             # Transcript #3
-            ['1', '.', 'transcript', '600', '799', '.', '+', '.', attrs(gid='G2', tid='T3')],
-            ['1', '.', 'exon', '600', '649', '.', '+', '.', attrs(gid='G2', tid='T3', exn=1)],
-            ['1', '.', 'CDS', '600', '649', '.', '+', '.', attrs(gid='G2', tid='T3', exn=1)],
-            ['1', '.', 'exon', '750', '799', '.', '+', '.', attrs(gid='G2', tid='T3', exn=2)],
-            ['1', '.', 'CDS', '750', '799', '.', '+', '.', attrs(gid='G2', tid='T3', exn=2)],
+            ['1', '.', 'transcript', '600', '899', '.', '+', '.', attrs(gid='G2', tid='T3')],
+            ['1', '.', 'exon', '600', '659', '.', '+', '.', attrs(gid='G2', tid='T3', exn=1)],
+            ['1', '.', 'CDS', '600', '659', '.', '+', '.', attrs(gid='G2', tid='T3', exn=1)],
+            ['1', '.', 'exon', '840', '899', '.', '+', '.', attrs(gid='G2', tid='T3', exn=2)],
+            ['1', '.', 'CDS', '840', '899', '.', '+', '.', attrs(gid='G2', tid='T3', exn=2)],
 
             # Gene #3, negative strand:
             ['1', '.', 'gene', '800', '899', '.', '-', '.', 'gene_id "G3";'],
             # Transcript #4
             ['1', '.', 'transcript', '800', '899', '.', '-', '.', attrs(gid='G3', tid='T4')],
             ['1', '.', 'exon', '800', '899', '.', '-', '.', attrs(gid='G3', tid='T4', exn=1)],
-            ['1', '.', 'CDS', '800', '899', '.', '-', '.', attrs(gid='G3', tid='T4', exn=1)],
         ])
 
         # Define positions of cross-links:
@@ -108,9 +106,12 @@ class TestEndToEnd(unittest.TestCase):
         From raw reads and ENSEMBL annotation to rnamaps.
         """
 
-        # Make segmentation
+        # Make segmentation & regions file
         seg = get_temp_file_name(extension='gtf')
+        out_dir = get_temp_dir()
         iCount.genomes.segment.get_segments(self.gtf, seg, self.fai)
+        iCount.genomes.region.make_regions(seg, out_dir)
+        regions = os.path.join(out_dir, iCount.genomes.region.REGIONS_FILE)
 
         # Build STAR index:
         genome_index = get_temp_dir()
@@ -126,50 +127,12 @@ class TestEndToEnd(unittest.TestCase):
         bam = [fname for fname in os.listdir(map_dir) if fname.startswith('Aligned')][0]
         bam = os.path.join(map_dir, bam)
 
-        # Make all sorts of analysis and save it:
-        normal_out = get_temp_file_name(extension='tsv')
-        strange_out = get_temp_file_name(extension='bam')
-        cross_tr_out = get_temp_file_name(extension='tsv')
-        iCount.analysis.rnamaps.run(bam, seg, normal_out, strange_out, cross_tr_out,
-                                    implicit_handling='split')
-        # Normal output:
-        expected_out = [
-            ['RNAmap', 'type', 'position', 'all', 'explicit'],
-            ['CDS-CDS', '-40', '0.3333', '0'],
-            ['CDS-UTR3', '-25', '0.25', '0'],
-            ['CDS-intergenic', '-20', '1', '1'],
-            ['CDS-intergenic', '30', '0.5', '0'],
-            ['CDS-intergenic', '250', '1', '0'],
-            ['CDS-intron', '-40', '0.3333', '0'],
-            ['CDS-intron', '-25', '0.25', '0'],
-            ['UTR5-CDS', '5', '0.25', '0'],
-            ['UTR5-intron', '-10', '1', '1'],
-            ['UTR5-intron', '-8', '2', '2'],
-            ['UTR5-intron', '10', '0.5', '0'],
-            ['UTR5-intron', '13', '1', '0'],
-            ['intergenic-CDS', '-70', '0.5', '0'],
-            ['intergenic-CDS', '10', '0.3333', '0'],
-            ['intergenic-UTR5', '-20', '1', '1'],
-            ['intron-CDS', '-40', '0.5', '0'],
-            ['intron-CDS', '-37', '1', '0'],
-            ['intron-CDS', '5', '0.25', '0'],
-        ]
-        self.assertEqual(expected_out, make_list_from_file(normal_out))
+        sites_single = get_temp_file_name(extension='bed.gz')
+        sites_multi = get_temp_file_name(extension='bed.gz')
+        skipped = get_temp_file_name(extension='bam')
+        iCount.mapping.xlsites.run(bam, sites_single, sites_multi, skipped)
 
-        # Cross transcript:
-        expected_cross_transcript = [
-            ['chrom', 'strand', 'xlink', 'second-start', 'end-position', 'read_len'],
-            ['1', '+', '234', '236', '284', '50'],
-        ]
-        self.assertEqual(expected_cross_transcript, make_list_from_file(cross_tr_out))
-
-        # Strange:
-        strange_reads = list(pysam.AlignmentFile(strange_out, 'rb'))  # pylint: disable=no-member
-        self.assertEqual(len(strange_reads), 1)
-        strange_read = strange_reads[0]
-        self.assertEqual(strange_read.query_name, 'name_strange:rbc:GGGG')
-        self.assertEqual(strange_read.reference_start, 250)
-        self.assertEqual(strange_read.cigar, [(0, 45), (2, 15), (0, 70)])
+        iCount.analysis.rnamaps.run(sites_single, regions)
 
 
 if __name__ == '__main__':

iCount/tests/test_plotting.py
@@ -1,0 +1,109 @@
+# pylint: disable=missing-docstring, protected-access
+import os
+import unittest
+import warnings
+
+from iCount.plotting import rnamap, rnaheatmap, rnacombined
+from iCount.tests.utils import get_temp_file_name, make_file_from_list
+
+
+def get_example_results_file():
+    """Produce some thest file."""
+    def zeros(size):
+        """Return array of zeros of size ``size``."""
+        return [0 for i in range(size)]
+
+    fname = make_file_from_list(bedtool=False, data=[
+        ['total_cdna:1000000'],
+        ['.'] + [str(pos) for pos in range(-50, 51)],
+        ['chr1_+_100'] + zeros(51) + ['3', '5'] + zeros(48),
+        ['chr1_+_200'] + zeros(49) + ['3', '0', '0', '1'] + zeros(48),
+    ])
+
+    return fname
+
+
+class TestPlotRnaMap(unittest.TestCase):
+
+    def setUp(self):
+        warnings.simplefilter("ignore", (ResourceWarning, ImportWarning))
+
+        self.results_file = get_example_results_file()
+
+    def test_normalize_cpm(self):
+
+        normalized = rnamap.normalize_cpm(1, 10**6)
+        self.assertEqual(normalized, 1)
+
+        normalized = rnamap.normalize_cpm(2, 10**6 * 2)
+        self.assertEqual(normalized, 1)
+
+    def test_parse(self):
+        data, landmark_count = rnamap.parse_results(self.results_file)
+        self.assertEqual(landmark_count, 2)
+
+        expected = {
+            -1: 3.0,
+            1: 3.0,
+            2: 6.0,
+        }
+        for pos, score in data.items():
+            self.assertEqual(score, expected.get(pos, 0.0))
+
+    def test_plot(self):
+        outfile = get_temp_file_name(extension='png')
+        rnamap.plot_rnamap(self.results_file, outfile)
+        self.assertTrue(os.path.isfile(outfile))
+
+
+class TestSmooth(unittest.TestCase):
+
+    def setUp(self):
+        warnings.simplefilter("ignore", (ResourceWarning, ImportWarning))
+
+    def test_smoothe(self):
+        # pylint: disable=missing-docstring, protected-access
+        values = [0, 2, 0]
+        self.assertEqual(rnamap.smooth(values, 1), [1, 2 / 3, 1])
+
+        values = [0, 2, 4, 1, 0]
+        self.assertEqual(rnamap.smooth(values, 1), [1, 2, 7 / 3, 5 / 3, 0.5])
+        self.assertEqual(rnamap.smooth(values, 2), [2, 7 / 4, 7 / 5, 7 / 4, 5 / 3])
+
+
+class TestPlotRnaHeatMap(unittest.TestCase):
+
+    def setUp(self):
+        warnings.simplefilter("ignore", (ResourceWarning, ImportWarning))
+
+        self.results_file = get_example_results_file()
+
+    def test_make_position_to_bin1(self):
+        pos2bins = rnaheatmap.make_position_to_bin([0, 2, 4])
+        self.assertEqual(pos2bins, {0: 0, 1: 0, 2: 2, 3: 2, 4: 2})
+
+    def test_parse(self):
+        data = rnaheatmap.parse_results(self.results_file, -50, 50, top_n=2, binsize=10)
+        self.assertEqual(list(data.index), ['chr1_+_100', 'chr1_+_200'])
+        self.assertEqual(list(data.columns), list(range(-50, 50, 10)))
+        self.assertEqual(data.at['chr1_+_100', 0], 8.0)
+        self.assertEqual(data.at['chr1_+_200', -10], 3.0)
+        self.assertEqual(data.at['chr1_+_200', 0], 1.0)
+
+    def test_plot(self):
+        outfile = get_temp_file_name(extension='png')
+        rnaheatmap.plot_rnaheatmap(self.results_file, outfile, top_n=2, binsize=10)
+        self.assertTrue(os.path.isfile(outfile))
+
+
+class TestPlotRnaCombined(unittest.TestCase):
+
+    def setUp(self):
+        warnings.simplefilter("ignore", (ResourceWarning, ImportWarning))
+
+        self.results_file = get_example_results_file()
+
+    def test_plot(self):
+        outfile = get_temp_file_name(extension='png')
+        rnacombined.plot_combined(self.results_file, outfile, top_n=2, nbins=50)
+        self.assertTrue(os.path.isfile(outfile))